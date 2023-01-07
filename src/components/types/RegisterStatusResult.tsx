3ef217f4dcacac8c7550cd811ff5537b402a0011
aoiro27 <aoiro.savant@gmail.com>
2023-01-07 21:16:33 +0900

    ip制限追加

--- src/components/types/RegisterStatusResult.tsx
+++ src/components/types/RegisterStatusResult.tsx
@@ -1,13 +1,12 @@
 export type RegisterStatusResult = {
-    registerdMail: string;
-    oneUid: string;
-    isPaidUser: boolean;
-    registerItems: RegisterItem[];
+  registerdMail: string
+  oneUid: string
+  isPaidUser: boolean
+  registerItems: RegisterItem[]
 }
 
 export type RegisterItem = {
-    tagName: string;
-    deliveryMail: string;
-    isAuthenticated: boolean;
-    
+  tagName: string
+  deliveryMail: string
+  isAuthenticated: boolean
 }
